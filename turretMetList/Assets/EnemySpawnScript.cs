023296250131ffe279e2f3e92e6551fb9c6f9825
Kramboja <koen5067@icloud.com>
2014-12-08 08:40:12 +0100

    firstPush

---
+++ turretMetList/Assets/EnemySpawnScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnScript : MonoBehaviour {
+
+	public GameObject Enemy;
+
+
+	void OnGUI()
+	{
+		if(GUILayout.Button("Spawn random enemy"))
+		{
+			Instantiate (Enemy, this.transform.position, this.transform.rotation);
+		}
+	}
+}
